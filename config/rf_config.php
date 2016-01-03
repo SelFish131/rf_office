654e36727b65db936b4b03b385df1dc49fa4367d
SelFish131 <rfonline.fz@yandex.ru>
2016-01-04 02:47:28 +0300

    Change config

--- config/rf_config.php
+++ config/rf_config.php
@@ -1,4 +1,4 @@
 <?
-$server[0]='localhost|selfish|selfish123|rf_world_novus|rf_user|true';
+$server[0]='localhost|selfish|selfish123|rf_world|rf_user|true';
 $server[1]='localhost|root|1q1w1e|cp';
 ?>
